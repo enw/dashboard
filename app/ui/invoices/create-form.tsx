b17cd4028eef9edee76349c4d0b9df481ac32a66
Elliot Winard <github@caveteen.com>
2024-06-10 14:57:00 -0400

    lesson 12 - create server-side route to insert into postgres

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,6 +7,8 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
+import { create } from 'domain';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
@@ -105,7 +107,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         >
           Cancel
         </Link>
-        <Button type="submit">Create Invoice</Button>
+        <Button type="submit" formAction={createInvoice}>Create Invoice</Button>
       </div>
     </form>
   );
